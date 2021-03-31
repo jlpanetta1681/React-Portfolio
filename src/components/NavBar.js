178b62e6903ffdd276cf094580451e847f94591b
Joe Panetta <jlapnetta1681@icloud.com>
2021-03-30 23:53:38 -0400

    working on the components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,13 +13,16 @@ export default function NavBar() {
                             className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl fint-bold cursive tracking-widest" >
                             Joseph
                         </NavLink>
-                        <NavLink to="/post">
+                        <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover: txt-green-800"
+                            activeClassName="text-red-100 bg-red-700">
                             Blog Posts
                         </NavLink>
-                        <NavLink to="/project">
+                        <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover: txt-green-800"
+                            activeClassName="text-red-100 bg-red-700">
                             Projects
                         </NavLink>
-                        <NavLink to="/about" >
+                        <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover: txt-green-800"
+                            activeClassName="text-red-100 bg-red-700">
                             About Me!
                         </NavLink >
 
