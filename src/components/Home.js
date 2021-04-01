55a14da331ce1260483aa050d550231c90b00cb8
Joe Panetta <jlapnetta1681@icloud.com>
2021-03-31 21:55:32 -0400

    got the words on the backgroundf starting on the next section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,15 @@ import image from "../images.jpg"
 export default function Home() {
     return (
         <main>
-            <div>
-
-                <img
-                    src={image}
-                    alt="KH monster"
-                    className="absolute object-cover w-full h-full" />
-                <section className="relative-flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                    <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name" >
-                        Hello My Name Is Joe!!
-                </h1>
+            <img
+                src={image} alt="Italy" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text 6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name" >
+                    Hello I'm Joe!! </h1>
 
 
-                </section>
-            </div>
+            </section>
+
         </main >
     )
 }
